99c9f76933454a1119ab5af7d5ac0c3402b7a085
Dumer <dumeti@gmail.com>
2024-07-11 21:52:57 +0500

    Homework start

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("Задача №1");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
